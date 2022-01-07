ada5a3a62c61ca615c5e99e6035f726e0a841df2
Maxime Evangeliste <max.evangeliste@gmail.com>
2022-01-06 20:31:43 -0500

    Install and configure capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.miloguide.capacitorjs',
+  appName: 'milo-capacitor',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
