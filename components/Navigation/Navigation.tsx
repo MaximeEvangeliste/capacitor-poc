0bdb37f7a9940a351a5c5519bb3522d509e035ea
Maxime Evangeliste <max.evangeliste@gmail.com>
2022-01-10 15:44:53 -0500

    Navigation base component

---
+++ components/Navigation/Navigation.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+type LinkProps = {
+  href: string;
+};
+
+const NavLink: React.FC<LinkProps> = ({ href, ...props }) => {
+  return (
+    <Link href={href}>
+      <a className="cursor-pointer mr-3 text-lg underline" {...props}></a>
+    </Link>
+  );
+};
+
+export const Navigation: React.FC = () => {
+  return (
+    <nav>
+      <NavLink href="/">Home</NavLink>
+      <NavLink href="/about">About</NavLink>
+    </nav>
+  );
+};
